3b732df7b04dd2f98d6f4c76a49add97a1931552
Sam Klingner <stk1@me.com>
2025-04-06 17:25:54 +0200

    Debug #3

---
+++ python_version/stiebel_control/__main__.py
@@ -0,0 +1,10 @@
+"""
+Main entry point for the stiebel_control package.
+
+This file allows running the package with python -m stiebel_control
+"""
+
+from stiebel_control.main import main
+
+if __name__ == '__main__':
+    main()
